b0b6225ba5c9d21541b9fe9396836db09dc9de89
shubham_364 <shubhamashish364@gmail.com>
2020-07-11 19:59:53 +0530

    Added more functionality to Gear class Added gear_inches() method.

--- bicycle.java
+++ bicycle.java
@@ -8,20 +8,29 @@ import java.util.*;
 class Gear{
 	private int chainRing;
 	private int cog;
+	private int rim;
+	private double tire;
 
-	public Gear(int chainRing, int cog){
+	Gear(int chainRing, int cog, int rim, double tire){
 		this.chainRing = chainRing;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 
 	public double ratio(){
 		return chainRing / (double)cog;
 	}
+
+	public double gear_inches(){
+		return ratio() * (rim + (tire * 2));
+	}
 }
 
 public class bicycle{
 	public static void main(String[] args){
-		Gear obj = new Gear(52, 11);
-		System.out.println(obj.ratio());
+		//Gear obj = new Gear(52, 11); won't work now
+		Gear obj = new Gear(52, 11, 26, 1.5);
+		System.out.println(obj.gear_inches());
 	}
 }
