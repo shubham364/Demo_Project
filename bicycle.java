c44092d23fbf7dbe6a31359293e77b46ae5a86e6
shubham_364 <shubhamashish364@gmail.com>
2020-07-15 19:38:14 +0530

    Implemented and tested a Wheel class.

--- bicycle.java
+++ bicycle.java
@@ -11,7 +11,7 @@ class Gear{
 	private int rim;
 	private double tire;
 
-	Gear(int chainRing, int cog, int rim, double tire){
+	public Gear(int chainRing, int cog, int rim, double tire){
 		this.chainRing = chainRing;
 		this.cog = cog;
 		this.rim = rim;
@@ -27,10 +27,32 @@ class Gear{
 	}
 }
 
+class Wheel{
+	private int rim;
+	private double tire;
+
+	public Wheel(int rim, double tire){
+		this.rim = rim;
+		this.tire = tire;
+	}
+
+	public double diameter(){
+		return rim + (tire * 2.0);
+	}
+
+	public double circumference(){
+		return diameter() * Math.PI;
+	}
+}
+
 public class bicycle{
 	public static void main(String[] args){
 		//Gear obj = new Gear(52, 11); won't work now 
-		Gear obj = new Gear(52, 11, 26, 1.5);
-		System.out.println(obj.gear_inches());
+		// Gear obj = new Gear(52, 11, 26, 1.5);
+		// System.out.println(obj.gear_inches());
+		// obj = new Gear(52, 11, 25, 1.5);
+		// System.out.println(obj.gear_inches());
+		Wheel mrf = new Wheel(26, 1.5);
+		System.out.println(mrf.circumference());
 	}
 }
