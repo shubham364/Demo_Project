4d22efbac48d59a1d36ae3793de1ae027d5085ef
shubham_364 <shubhamashish364@gmail.com>
2020-07-15 19:57:17 +0530

    Modified Gear class. Used object of Wheel class to add a dependency relation.

--- bicycle.java
+++ bicycle.java
@@ -5,28 +5,6 @@ package learn;
 
 import java.util.*;
 
-class Gear{
-	private int chainRing;
-	private int cog;
-	private int rim;
-	private double tire;
-
-	public Gear(int chainRing, int cog, int rim, double tire){
-		this.chainRing = chainRing;
-		this.cog = cog;
-		this.rim = rim;
-		this.tire = tire;
-	}
-
-	public double ratio(){
-		return chainRing / (double)cog;
-	}
-
-	public double gear_inches(){
-		return ratio() * (rim + (tire * 2));
-	}
-}
-
 class Wheel{
 	private int rim;
 	private double tire;
@@ -45,6 +23,30 @@ class Wheel{
 	}
 }
 
+class Gear {
+	private int chainRing;
+	private int cog;
+	Wheel obj = null;
+
+	public Gear(int chainRing, int cog){
+		this.chainRing = chainRing;
+		this.cog = cog;
+		//this.obj = obj;
+	}
+
+	public void setObj(Wheel obj){
+		this.obj = obj;
+	}
+
+	public double ratio(){
+		return chainRing / (double)cog;
+	}
+
+	public double gear_inches(){
+		return ratio() * obj.diameter();
+	}
+}
+
 public class bicycle{
 	public static void main(String[] args){
 		//Gear obj = new Gear(52, 11); won't work now 
@@ -54,5 +56,9 @@ public class bicycle{
 		// System.out.println(obj.gear_inches());
 		Wheel mrf = new Wheel(26, 1.5);
 		System.out.println(mrf.circumference());
+		Gear obj = new Gear(52, 11);
+		obj.setObj(mrf);
+		System.out.println(obj.gear_inches());
+		System.out.println(obj.ratio());
 	}
 }
